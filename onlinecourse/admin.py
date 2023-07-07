86f97eda8d7a7a9901e06fd9752978aeee519168
Adekunle Adesokan <kunle.adesokan9@gmail.com>
2023-07-07 11:34:33 -0400

    final update

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -5,6 +5,14 @@ from .models import Course, Lesson, Instructor, Learner, Question, Choice
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
 
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 5
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 4
+
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
@@ -21,11 +29,9 @@ class CourseAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
-class QuestionInline(admin.ModelAdmin):
-    model = Question
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ ChoiceInline]
 
-class ChoiceInline(admin.ModelAdmin):
-    model = Choice
 
 # <HINT> Register Question and Choice models here
 
@@ -33,5 +39,6 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
-admin.site.register(Question)
-admin.site.register(Choice)
+
+admin.site.register(Question, QuestionAdmin)
+#admin.site.register(Choice)
--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('<int:pk>/', views.CourseDetailView.as_view(), name='course_details'),
     # ex: /enroll/5/
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
-    path('<int:course_id>/submit/', submit, name='submit'),
+    path('<int:course_id>/submit/', views.submit, name='submit'),
     path('course/<int:course_id>/result/<int:submission_id>/', show_exam_result, name='show_exam_result'),
 
  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
